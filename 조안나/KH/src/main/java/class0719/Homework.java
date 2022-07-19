970a513752b5568c567364689b93472b91eb6b5c
joanna930224 <annavu0224@gmail.com>
2022-07-19 23:35:06 +0900

    [조안나] 과제1

---
+++ 조안나/KH/src/main/java/class0719/Homework.java
@@ -0,0 +1,13 @@
+package class0719;
+
+public class Homework {
+    public static void main(String[] args) {
+        System.out.println("강아지!");
+        System.out.println();
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
