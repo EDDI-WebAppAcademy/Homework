eabc81f4f151da96e3c2d785c50580c625f6e98f
gkghgi <gkghgi3@naver.com>
2022-07-21 16:00:16 +0900

    [김종찬]2022/07/20 2차 과제 2

--- 김종찬/spring/homeworks1/src/main/java/kr/eddi/demo/basic/secondHomeWork.java
+++ 김종찬/spring/homeworks1/src/main/java/kr/eddi/demo/basic/secondHomeWork.java
@@ -9,7 +9,7 @@ import java.util.Random;
 public class secondHomeWork {
 
     @GetMapping("/")
-    public String index() {
+    public String dice() {
         Random random = new Random();
         int result = random.nextInt(6)+1;
         return "당신의 숫자 : "+result;
