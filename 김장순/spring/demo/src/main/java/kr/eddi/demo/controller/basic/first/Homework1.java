791a58e388cb8f7f4765972cacd3044bae6da890
jssnk <ssnssnl@kakao.com>
2022-07-21 16:04:48 +0900

    [김장순]2022/07/20 두번째과제

--- 김장순/spring/demo/src/main/java/kr/eddi/demo/controller/basic/first/Homework1.java
+++ 김장순/spring/demo/src/main/java/kr/eddi/demo/controller/basic/first/Homework1.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Random;
 
 @RestController
-public class homework1 {
+public class Homework1 {
 
     @GetMapping("/homework1")
-    public String Dice(){
+    public String dice(){
         Random random = new Random();
         int result = random.nextInt(6)+1;
         return "주사위를 굴려 "+result+"이 나왔습니다.";
