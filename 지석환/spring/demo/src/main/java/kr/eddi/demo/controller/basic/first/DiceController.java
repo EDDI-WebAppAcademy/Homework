59fca75c35be607a28feb705e7e029e3c58b2649
Ji_Seok_Hwan <addy9883@gmail.com>
2022-07-22 21:08:20 +0900

    [지석환] GUI 환경에서 커밋 합시다.

---
+++ 지석환/spring/demo/src/main/java/kr/eddi/demo/controller/basic/first/DiceController.java
@@ -0,0 +1,15 @@
+package kr.eddi.demo.controller.basic.first;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class DiceController {
+
+    @GetMapping("/homework1")
+    public String rollDice() {
+        return "무조건 6이 나오는 주사위입니다!";
+    }
+}
