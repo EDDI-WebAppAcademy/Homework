6a26539057a4a6c0c9c9bdb0a9dc0770532b6376
HEEJI YUN <yhj8147e@gmail.com>
2022-07-22 11:31:20 +0900

    [윤희지] 2022/07/21 3번째 과제

--- 윤희지/spring/homework/src/main/java/com/example/homework/first/controller/DiceController.java
+++ 윤희지/spring/homework/src/main/java/com/example/homework/first/controller/DiceController.java
@@ -1,16 +1,14 @@
-package com.example.homework.Controller;
+package com.example.homework.first.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.function.IntSupplier;
-
 @RestController
-public class HW_RestController {
+public class DiceController {
 
     @GetMapping("/homework1")
-    public int dice() {
+    public int rollDice() {
             return (int) (Math.random()*6) + 1;
     }
 }
