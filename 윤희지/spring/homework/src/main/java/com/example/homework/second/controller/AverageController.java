83c3847482e08f1d894de7bb19664f24f3aadf4e
HEEJI YUN <yhj8147e@gmail.com>
2022-07-25 15:22:23 +0900

    [윤희지] 2022/07/22 4번째 과제

--- 윤희지/spring/homework/src/main/java/com/example/homework/second/controller/AverageController.java
+++ 윤희지/spring/homework/src/main/java/com/example/homework/second/controller/AverageController.java
@@ -1,6 +1,6 @@
 package com.example.homework.second.controller;
 
-import com.example.homework.second.entity.ClassAverage;
+import com.example.homework.second.entity.ClassRoom;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +15,9 @@ public class AverageController {
     public String responseClassAverage() {
         log.info("responseClassAverage() 동작");
 
-        ClassAverage classAvg = new ClassAverage(30, 60);
-        classAvg.randomScore();
+        ClassRoom classAvg = new ClassRoom(30, 60);
+        classAvg.makeStudent();
+        classAvg.doExam();
 
         return classAvg.getStudentNum() + "명 반평균은 " + classAvg.calAvg() + "입니다.";
     }
