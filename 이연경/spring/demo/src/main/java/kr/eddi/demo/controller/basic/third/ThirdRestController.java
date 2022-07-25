37b00a3f0dd95466422e88a05af0180aa532baf7
k724k <k724k@naver.com>
2022-07-25 14:38:09 +0900

    [이연경]2022/07/22 4번째 과제

---
+++ 이연경/spring/demo/src/main/java/kr/eddi/demo/controller/basic/third/ThirdRestController.java
@@ -0,0 +1,30 @@
+package kr.eddi.demo.controller.basic.third;
+
+
+import kr.eddi.demo.entity.basic.third.ClassRoom;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/third")
+
+
+public class ThirdRestController {
+
+    @GetMapping("/homework")
+    public float calcClassRoomMeanScore() {
+        log.info("학급의 평균을 계산합니다: calcClassRoomMeanScore()");
+
+        ClassRoom classroom = new ClassRoom();
+        classroom.setRoomName("빅데이터 반");
+        classroom.makeRandomStudents();
+        classroom.takeTest(60);
+
+        return classroom.calcMean();
+
+
+    }
+}
