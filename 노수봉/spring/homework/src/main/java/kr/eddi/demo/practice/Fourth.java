24b357673fb259c3a593593cde415f6492a4a92e
su940412 <su940412@gmail.com>
2022-07-25 15:03:29 +0900

    [노수봉]2022/07/22 네번째 과제

---
+++ 노수봉/spring/homework/src/main/java/kr/eddi/demo/practice/Fourth.java
@@ -0,0 +1,13 @@
+package kr.eddi.demo.practice;
+
+public class Fourth {
+    public static void main(String[] args) {
+
+        int num = 7;
+        System.out.println("7단을 출력해보자!");
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("7 * "+ i + " = " + (num * i));
+        }
+    }
+}
