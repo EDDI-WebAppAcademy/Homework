56813e1181914fc3ecd379f47b2f30463dd3b6fa
Hanchaeseon <mdmodina34@gmail.com>
2022-07-26 16:18:14 +0900

    [한채선] 2022/07/26 다섯번째 과제 - 문제은행

--- 한채선/spring/July20RandomDice/src/main/java/kr/eddi/demo/controller/fourth/SequenceAndLoopController.java
+++ 한채선/spring/July20RandomDice/src/main/java/kr/eddi/demo/controller/fourth/SequenceAndLoopController.java
@@ -5,8 +5,10 @@ import kr.eddi.demo.utility.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RestController
 @RequestMapping("/fourth")
 public class SequenceAndLoopController {
 
