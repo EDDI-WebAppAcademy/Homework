4f530acaf7d66e177a05f5cc9b49ce3cd431610d
Hanchaeseon <mdmodina34@gmail.com>
2022-07-26 16:16:57 +0900

    [한채선] 2022/07/26 다섯번째 과제 - 문제은행

---
+++ 한채선/spring/July20RandomDice/src/main/java/kr/eddi/demo/utility/AscciiAlphabet.java
@@ -0,0 +1,42 @@
+package kr.eddi.demo.utility;
+
+public class AscciiAlphabet {
+
+    // 65~90 까지 대문자
+    // 97~122 까지 소문자
+
+    private final int ASCII_ALP_MIN = 65;
+    private final int ASCII_ALP_MAX = 122;
+    private final int ASCII_CHAR_MIN = 33;
+    private final int ASCII_CHAR_MAX = 126;
+
+
+
+    /**
+     * 랜덤한 소문자와 대문자 알파벳을 출력하는 메소드
+     * @return char 알파벳
+     */
+    public char randomAlphabet() {
+        char resultABC;
+        while (true) {
+            int asciiNumber = RandomNumber.randomNum(ASCII_ALP_MIN, ASCII_ALP_MAX);
+            if (asciiNumber > 91 && asciiNumber < 96) {
+                continue;
+            } else {
+                resultABC = (char) asciiNumber;
+                break;
+            }
+        }
+        return resultABC;
+    }
+
+
+    /**
+     * 아스키 코드 범위 내의 랜덤한 character를 출력하는 메소드
+     * @return char 문자 or 알파벳
+     */
+    public char randomASCIIChar() {
+        char asciiChar = (char) RandomNumber.randomNum(ASCII_CHAR_MIN, ASCII_CHAR_MAX);
+        return asciiChar;
+    }
+}
---
+++ 한채선/spring/July20RandomDice/src/main/java/kr/eddi/demo/utility/TotalSum.java
@@ -0,0 +1,35 @@
+package kr.eddi.demo.utility;
+
+public class TotalSum extends DisplayNumbers {
+
+
+    /**
+     * 1~100 까지의 숫자 총합을 구하는 메서드
+     * @return
+     */
+    public int multipleSum() {
+        this.displayNums(1, 100);
+        return numList.stream().mapToInt(Integer::intValue).sum();
+    }
+
+
+    /**
+     * 최소값과 최대값을 설정하여 범위 내에 존재하는 지정된 숫자의 배수의 총합을 구하는 메서드
+     * @param min 최소
+     * @param max 최대
+     * @param multiple 배수
+     * @return
+     */
+    public int multipleSum(int min, int max, int multiple) {
+        displayNums(min, max, multiple);
+        return numList.stream().mapToInt(Integer::intValue).sum();
+    }
+
+
+
+    public int moveNumbersSum(int min, int max, int moveNum) {
+        displayMoveNumbers(min, max, moveNum);
+        return numList.stream().mapToInt(Integer::intValue).sum();
+    }
+
+}
