abf1fd8d65ebf57114cfaa13e0a603c5077010b5
jssnk <ssnssnl@kakao.com>
2022-07-27 14:17:19 +0900

    [김장순]2022/07/26 6번째 과제 작성중.. 완성x

---
+++ 김장순/spring/demo/src/main/java/kr/eddi/demo/entity/sixth/DiceGame.java
@@ -0,0 +1,75 @@
+package kr.eddi.demo.entity.sixth;
+
+import kr.eddi.demo.utility.basic.third.CustomRandom;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class DiceGame {
+    private final int MIN = 1;
+    private final int MAX = 6;
+    private int participants;
+    private final List<Integer> sumArr = new ArrayList<>(participants);
+
+    public DiceGame(int participants) {
+
+        this.participants = participants;
+    }
+
+    public List<Integer> firstRoll() {
+
+        for (int i = 0; i < sumArr.size(); i++) {
+            sumArr.add(CustomRandom.makeIntCustomRandom(MIN, MAX));
+        }
+        return sumArr;
+    }
+
+    public List<Integer> secondRoll() {
+
+        int throwSum = 0;
+        for (int i = 0; i < sumArr.size(); i++) {
+            if (sumArr.get(i) % 2 == 0) {
+                int secondNum = CustomRandom.makeIntCustomRandom(MIN, MAX);
+                if (secondNum == 1) {
+                    sumArr.set(i, sumArr.get(i) + 1);
+                    for (int j = 0; j < sumArr.size(); j++) {
+                        if (sumArr.get(j) >= 2) {
+                            sumArr.set(j, sumArr.get(j) - 2);
+                        } else if (sumArr.get(j) == 1) { //2점 미만일 때는 1점만 감점
+                            sumArr.set(j, sumArr.get(j) - 1);
+                        }
+                    }
+                } else if (secondNum == 3) {
+                    sumArr.set(i, sumArr.get(i) + 3);
+                    for (int j = 0; j < sumArr.size(); j++) {
+                        if (j != i) {
+                            if (sumArr.get(j) >= 3) { //3점 이상인 친구에게만 3점씩 가져오기
+                                sumArr.set(j, sumArr.get(j) - 3);
+                                throwSum += 3;
+                            }
+                        }
+                    }
+                    sumArr.set(i, sumArr.get(i) + throwSum);
+                } else if (secondNum == 4) {
+                    sumArr.set(i, sumArr.get(i) + 4);
+                    break;
+                } else if (secondNum == 5) {
+                    sumArr.set(i, sumArr.get(i) + (5 - 2)); //2씩 나누어 주어야함, 한번만 모두에게 나누어줌
+                    for (int j = 0; j < sumArr.size(); j++) {
+                        if (j != i) {
+                            sumArr.set(j, sumArr.get(j) + 2);
+                        }
+                    }
+
+                } else {
+                    sumArr.set(i,sumArr.get(i)+secondNum);
+                }
+            } else {
+
+            }
+        }
+        return sumArr;
+    }
+}
---
+++ 김장순/spring/demo/src/main/java/kr/eddi/demo/entity/sixth/MaxFinder.java
@@ -0,0 +1,21 @@
+package kr.eddi.demo.entity.sixth;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MaxFinder {
+    public int findMaxScore(List<Integer> list){
+
+        int maxScore = Collections.max(list);
+
+        return maxScore;
+    }
+    public int findMaxScorePlayer(List<Integer> list){
+
+        int maxScore = Collections.max(list);
+        int indexNum = list.indexOf(maxScore);
+
+        return indexNum;
+    }
+}
