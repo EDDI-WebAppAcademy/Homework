30a9a846695fe672ecd0b82a6156fdfe5188a61a
kim-jung-ho <bmp3124@gmail.com>
2022-07-27 15:28:17 +0900

    [김중호] 2022/07/26 6번째 과제

---
+++ 김중호/spring/demo/src/main/java/kr/eddi/demo/controller/homework/sixth/bank4.java
@@ -0,0 +1,14 @@
+package kr.eddi.demo.controller.homework.sixth;
+
+import java.util.Arrays;
+
+public class bank4 {
+
+public static void main(String[] args) {
+    int[][] arr = {
+            {10, 20},
+            {30, 40}
+    };
+            System.out.println(Arrays.deepToString(arr));
+    };
+}
---
+++ 김중호/spring/demo/src/main/java/kr/eddi/demo/entity/basic/sixth/KhStudents.java
@@ -0,0 +1,11 @@
+package kr.eddi.demo.entity.basic.sixth;
+
+public class KhStudents {
+
+        public static int mathScore; // 수학 점수
+        public static int englishScore; // 영어 점수
+        public static int koreanScore; // 국어 점수
+        public static float average; // 평균
+        public static double dev; // 분산
+        public static float standardDeviation; // 표준편차
+}
