36a0252c409ac7e2472c48b0d43827151df2f4d1
kevvblog <kevvblog@gmail.com>
2022-07-27 15:23:01 +0900

    [공필석] 2022/07/26 6번째 과제

---
+++ 공필석/spring/demo/src/main/java/kr/eddi/demo/entity/basic/sixth/Cat.java
@@ -0,0 +1,5 @@
+package kr.eddi.demo.entity.basic.sixth;
+
+public class Cat {
+
+}
---
+++ 공필석/spring/demo/src/main/java/kr/eddi/demo/entity/basic/sixth/DoubleArray.java
@@ -0,0 +1,11 @@
+package kr.eddi.demo.entity.basic.sixth;
+
+import java.util.Arrays;
+
+public class DoubleArray {
+
+    public static void main(String[] args) {
+        int[][] doubleArr = {{1,2,3}, {4,5,6}};
+        System.out.println(Arrays.deepToString(doubleArr));
+    }
+}
