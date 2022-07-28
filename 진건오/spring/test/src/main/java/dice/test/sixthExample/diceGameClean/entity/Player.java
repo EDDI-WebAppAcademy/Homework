3f82462c6811bc14ff1d31b84bfa288d19583af2
G <shjwin96@gmail.com>
2022-07-28 13:42:05 +0900

    [진건오] 2022/07/22 여섯번째 과제(수업내용반영 코드수정) feat: [DICE-GAME]

---
+++ 진건오/spring/test/src/main/java/dice/test/sixthExample/diceGameClean/entity/Player.java
@@ -0,0 +1,34 @@
+package dice.test.sixthExample.diceGameClean.entity;
+
+import dice.test.sixthExample.diceGameClean.utillity.Dice;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Player {
+
+    private Score score;
+
+    private String playerName;
+
+    private String defaultName = "player";
+
+    public Player() {
+        playerName = defaultName;
+        score = new Score();
+    }
+
+    public void startDiceGame(Player[] player) {
+        for (int i = 0; i < player.length; i++) {
+            int diceNum = Dice.rollDice();
+            score.calcScore(diceNum);
+
+            if (diceNum % 2 == 0) {
+                diceNum = Dice.rollDice();
+                Dice effectDice = new Dice();
+                effectDice.effectDiceNumApply(player,playerName, diceNum, i);
+            }
+        }
+    }
+}
