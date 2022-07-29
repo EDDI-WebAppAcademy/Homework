a7f8e6f366370610b1a3d7e25efc7c3f237e1249
JBTV1 <ljb8194@gmail.com>
2022-07-29 11:29:30 +0900

    [임재범]2022/07/28 7차과제(LOTTO-SYSTEM2)

---
+++ 임재범/spring/demo/src/main/java/kr/eddi/demo/entity/basic/practice7_28/LottoTicket.java
@@ -0,0 +1,34 @@
+package kr.eddi.demo.entity.basic.practice7_28;
+
+import kr.eddi.demo.utility.basic.third.CustomRandom;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class LottoTicket {
+    final int NUMOFLOTTO = 6;
+    int[] LottoNum = new int[NUMOFLOTTO]; //6칸짜리 배열생성.
+    private int finalScore; //최종점수
+
+    public void finalScoreCal(int[] LottoNum){
+        int sum=0;
+        int multiplier = LottoNum[LottoNum.length-2];//곱하는 수는 마지막에서 두번째
+        int divisor = LottoNum[LottoNum.length-1]; //나누는 수는 마지막 친구
+
+        for(int i=0; i<LottoNum.length-2; i++){
+            sum+=LottoNum[i];
+        }
+
+        this.finalScore = sum*multiplier/divisor; //최종결과를 finalScore에 저장.
+
+    }
+
+
+
+
+
+        }
+
+
+
+
