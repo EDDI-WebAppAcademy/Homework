4ff9d9ad8d6b1f454dbc05495f05aaade69c1cc0
kim-jung-ho <bmp3124@gmail.com>
2022-07-29 13:42:04 +0900

    [김중호]2022/07/29 8번째 과제

--- 김중호/spring/demo/src/main/java/kr/eddi/demo/controller/basic/fourth/FourthRestController.java
+++ 김중호/spring/demo/src/main/java/kr/eddi/demo/controller/basic/fourth/FourthRestController.java
@@ -6,10 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @Slf4j
 @RestController
 @RequestMapping("/fourth")
 public class FourthRestController {
+
     private final int FIBONACCI = 1;
     private final int CUSTOM = 2;
     private final int EXAMPLE2_DATA = 20;
@@ -27,6 +29,7 @@ public class FourthRestController {
     @GetMapping("/find-fibonacci-series")
     public String findFibonacciSeries() {
         log.info("피보나치 수열을 검색합니다.");
+
         Series series = new Series(FIBONACCI, EXAMPLE2_DATA);
         return "피보나치 수열의 " + EXAMPLE2_DATA + "번째 항은 "
                 + series.findSeriesNum();
@@ -37,6 +40,7 @@ public class FourthRestController {
     @GetMapping("/find-custom-series")
     public String findCustomSeries() {
         log.info("커스텀 수열을 검색합니다.");
+
         Series series = new Series(CUSTOM, EXAMPLE3_DATA);
         return "커스텀 수열의 " + EXAMPLE3_DATA + "번째 항은 "
                 + series.findSeriesNum();
