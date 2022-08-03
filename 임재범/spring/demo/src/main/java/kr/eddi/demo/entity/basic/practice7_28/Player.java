46232c76a723ea0b7184b23191d057a5a54d890c
JBTV1 <ljb8194@gmail.com>
2022-08-03 14:44:25 +0900

    [임재범]2022/08/03 7차 로또게임(주석달면서 복습했습니다.-Player가 빠져서 추가커밋)

--- 임재범/spring/demo/src/main/java/kr/eddi/demo/entity/basic/practice7_28/Player.java
+++ 임재범/spring/demo/src/main/java/kr/eddi/demo/entity/basic/practice7_28/Player.java
@@ -1,20 +1,25 @@
 package kr.eddi.demo.entity.basic.practice7_28;
 
-import java.util.ArrayList;
+import kr.eddi.demo.entity.basic.practice7_28.LottoCard;
+import kr.eddi.demo.entity.basic.practice7_28.LottoCardManager;
+import kr.eddi.demo.utility.basic.practice7_28.NicknameGenerator;
+import lombok.Getter;
+import lombok.ToString;
 
-//100명의 플레이어는 모두 6개의 로또 번호를 가져야하고, n번째 번호는 겹치는것이 없어야 한다.
-//배열안에 또다시 6칸짜리 배열이 들어가는 형태?
+@Getter
+@ToString
 public class Player {
+    private LottoCard card; //플레이어는 LOttoCard를 갖습니다. 이 LottoCard 안에는 로또번호를 적을 int형 배열이 담겨있습니다.
+    private String nickName;//닉네임.
 
-    private int finalScore; //플레이어는 최종점수를 갖는다.
-    final int NUMOFPLAYER = 100;
-    final int NUMOFLOTTO = 6;
-
-
-    public void finalScoreCal(LottoTicket lottoTicket) {
-
-
+    //Player객체 생성하면 NicknameGenerator에 의해 랜덤닉네임이 중복되지 않게 생성됩니다.
+    public Player() {
+        nickName = NicknameGenerator.generateRandomNickname();
+    }
 
 
+    //LottoCardManager는 LottoCard들을 관리 할 수 있습니다. 이 메서드를 통해 미리 생성해둔, 6개번호가 부여된 로또를 플레이어에게 전달합니다.
+    public void acquireLottoCard(LottoCardManager manager, int idx) {
+        card = manager.getCards().get(idx);
     }
 }
