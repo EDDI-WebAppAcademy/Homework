b582d44cf2763b0d7383369edaeea726285c3163
dltkfla <yusohou@gmail.com>
2022-08-24 17:44:45 +0900

    [홍수연] 밀린게 많아서 뭐가 뭔지 구별이 안되는 상황이 발생함.

--- 홍수연/spring/demo/src/main/java/kr/eddi/demo/controller/homework/StudentExamAverage.java
+++ 홍수연/spring/demo/src/main/java/kr/eddi/demo/controller/homework/StudentExamAverage.java
@@ -3,9 +3,8 @@ package kr.eddi.demo.controller.homework;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import java.util.Random;
+
 
 @Slf4j
 @Controller
---
+++ 홍수연/spring/demo/src/main/java/kr/eddi/demo/entity/basic/seventh/Player.java
@@ -0,0 +1,49 @@
+package kr.eddi.demo.entity.basic.seventh;
+import kr.eddi.demo.utility.basic.seventh.Dice;
+import lombok.Getter;
+import lombok.ToString;
+import java.util.List;
+@Getter
+@ToString
+public class Player implements Comparable<Player> {
+    private Score score;
+    private String nickName;
+    private final String DEFAULT = "anonymous";
+    public Player() {
+        nickName = DEFAULT;
+        score = new Score();
+    }
+    public void gameStart(List<Player> players) {
+        for (int i = 0; i < players.size(); i++) {
+            int diceNum = Dice.rollDice();
+            score.addScore(diceNum);
+            if (diceNum % 2 == 0) {
+                diceNum = Dice.rollDice();
+                Dice specialDice = new Dice();
+                score.addScore(diceNum);
+                specialDice.checkSpecialDice(players, diceNum, i);
+            }
+        }
+    }
+
+    // 코드 단위로 분석하면 대가리 폭발하므로
+    // 쉽게 생각한다면 아래와 같은 메커니즘으로 보도록 한다.
+    // 1. 내가 비교하고자 하는 정보가 무엇인가 ? 현재 우리케이스는 totalScore
+    //    그래서 비교 대상에 this.getScore().getTotalScore(),
+    //    player.getScore().getTotalScore()이 사용됨
+    // 2. 다음으로 한쪽이 크면 양수, 작으면 음수, 같으면 0으로 배치하면
+    //    내가 비교하고자 하는 값으로 언제든지 정렬이됨
+    // 3. 내부 코드 볼 필요 없음 (추상화)
+    @Override
+    public int compareTo(Player player) {
+        int srcScore = this.getScore().getTotalScore();
+        int dstScore = player.getScore().getTotalScore();
+        if (srcScore > dstScore) {
+            return 1;
+        } else if (srcScore < dstScore) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
