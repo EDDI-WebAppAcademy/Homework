345f25478a4e922b0186a249887f2a52bb742b18
HEEJI YUN <yhj8147e@gmail.com>
2022-08-29 16:27:03 +0900

    vue frontend directory 통커밋

---
+++ 윤희지/frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/test">Test</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<scopedstyle>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</scopedstyle>
