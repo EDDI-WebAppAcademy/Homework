18f83a4ce52318312ee1a63903289a957eed227f
JBTV1 <ljb8194@gmail.com>
2022-08-31 18:05:29 +0900

    [임재범] 2022-08-31 vue실습(mon 변수에 0~20사이의 정수를 선언해서 그것을 RandomMonsterComponent에 있는 random-number로 전달하고 그 번호에 해당하는 idx의 몬스터를 꺼내는 식으로 만듦.)

---
+++ 임재범/frontend/src/components/basic/RandomMonsterComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    {{ this.monsterBooks[randomNumber] }}
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "RandomMonsterComponent",
+  props: {
+    randomNumber: Number,
+  },
+  data() {
+    return{
+      monsterBooks: [
+        { monsterId: 1, name: '슬라임', hp: 50, exp: 10, dropMoney: 5 },
+        { monsterId: 2, name: '고블린', hp: 100, exp: 20, dropMoney: 10 },
+        { monsterId: 3, name: '놀', hp: 200, exp: 40, dropMoney: 20 },
+        { monsterId: 4, name: '멧돼지', hp: 400, exp: 100, dropMoney: 20 },
+        { monsterId: 5, name: '스켈레톤', hp: 350, exp: 90, dropMoney: 10 },
+        { monsterId: 6, name: '오크', hp: 550, exp: 150, dropMoney: 40 },
+        { monsterId: 7, name: '홉고블린', hp: 650, exp: 180, dropMoney: 50 },
+        { monsterId: 8, name: '스켈레톤 아처', hp: 300, exp: 150, dropMoney: 50 },
+        { monsterId: 9, name: '트롤', hp: 800, exp: 200, dropMoney: 80 },
+        { monsterId: 10, name: '좀비', hp: 350, exp: 90, dropMoney: 50 },
+        { monsterId: 11, name: '고블린 매지션', hp: 700, exp: 500, dropMoney: 300 },
+        { monsterId: 12, name: '오우거', hp: 1000, exp: 250, dropMoney: 50 },
+        { monsterId: 13, name: '고스트', hp: 750, exp: 300, dropMoney: 1 },
+        { monsterId: 14, name: '골렘', hp: 2000, exp: 400, dropMoney: 100 },
+        { monsterId: 15, name: '와이번', hp: 3500, exp: 500, dropMoney: 150 },
+        { monsterId: 16, name: '리치', hp: 2000, exp: 1000, dropMoney: 500 },
+        { monsterId: 17, name: '스켈레톤 기사', hp: 2500, exp: 700, dropMoney: 100 },
+        { monsterId: 18, name: '웨어울프', hp: 5000, exp: 1000, dropMoney: 700 },
+        { monsterId: 19, name: '미노타우루스', hp: 10000, exp: 1500, dropMoney: 1000 },
+        { monsterId: 20, name: '드레이크', hp: 20000, exp: 5000, dropMoney: 50000 },
+        { monsterId: 21, name: '죽음의 군주', hp: 1000000, exp: 200000, dropMoney: 1000000 },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
