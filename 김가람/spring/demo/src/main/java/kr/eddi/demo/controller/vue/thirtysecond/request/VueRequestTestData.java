659dd6a25ab89271b10d59d7bb8d92282fc6ffd3
Karam Kim <kim.ram454@gmail.com>
2022-09-08 20:52:15 +0900

    [김가람] 2022/09/07 Vue To Spring - postMapping 수업 정리

---
+++ 김가람/spring/demo/src/main/java/kr/eddi/demo/controller/vue/thirtysecond/request/VueRequestTestData.java
@@ -0,0 +1,11 @@
+package kr.eddi.demo.controller.vue.thirtysecond.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class VueRequestTestData {
+    private String memberName;
+    private String major;
+}
