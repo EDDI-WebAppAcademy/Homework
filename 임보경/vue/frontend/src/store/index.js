eaaf3efa989ccb9331d8aae464637a2577c52aaf
tonowoods <tono.woods@gmail.com>
2022-09-18 19:52:46 +0900

    2022.09.18 RPG 게임 재도전

--- 임보경/vue/frontend/src/store/index.js
+++ 임보경/vue/frontend/src/store/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import state from "@/store/states"
+import actions from "@/store/actions"
+import mutations from "@/store/mutations"
+import getters from "@/store/getters"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-
+    state,
+    actions,
+    mutations,
+    getters,
 })
