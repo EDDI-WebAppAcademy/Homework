4d46324d233055fcffc6c867d96ae136c7f18c3d
G <shjwin96@gmail.com>
2022-09-21 17:33:22 +0900

    [진건오] 2022-09-21 상품 게시판 - 사용자의 목적에 따라 분류[PRODUCT-BOARD1]

---
+++ 진건오/vue/frontend/src/views/fourteenTh/porduct_board/EcommerceBoardView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="ecommerce" align="center">
+    <h2>🛒E-Commerce🛒</h2>
+    <h3>목적에 맞게 선택해주세요.</h3>
+    <button @click="clickToConsumerPage">고객 전용</button> |
+    <button @click="clickToSellerPage">판매자 전용</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EcommerceBoardView",
+
+  methods:{
+    async clickToConsumerPage() {
+      await this.$router.push({name: 'EcommerceConsumerListView' })
+    },
+
+    async clickToSellerPage() {
+      await this.$router.push({name: 'EcommerceSellerListView'})
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
