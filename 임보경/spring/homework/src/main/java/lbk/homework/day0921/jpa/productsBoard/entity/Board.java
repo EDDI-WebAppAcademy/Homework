dcae3c302d2e1ef4f914540ce353e63a1acfd5c6
tonowoods <tono.woods@gmail.com>
2022-09-21 15:58:50 +0900

    굿즈 판매 사이트 : 판매하는 굿즈 상품 게시판을 위한 엔티티 설계 [PRODUCTS-BOARD2]

---
+++ 임보경/spring/homework/src/main/java/lbk/homework/day0921/jpa/productsBoard/entity/Board.java
@@ -0,0 +1,36 @@
+package lbk.homework.day0921.jpa.productsBoard.entity;
+
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardNo;
+
+    @Column(length = 128, nullable = false)
+    private String productName;
+
+    @Column(length = 32, nullable = false)
+    private Integer productPrice;
+
+    @Lob
+    private String productDetails;
+
+    @CreationTimestamp
+    private Date regDate;
+
+    @UpdateTimestamp
+    private Date updDate;
+
+    @Column(length = 32, nullable = false)
+    private Integer NumberInStock;
+}
