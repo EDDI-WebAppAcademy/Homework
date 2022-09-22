8593bda221a1f6d2de70070467e8b240468db783
JBTV1 <ljb8194@gmail.com>
2022-09-22 19:52:44 +0900

    AespaListView빠뜨림

---
+++ 임재범/frontend/src/views/boards2/JpaAespaListView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="board">
+<h2>aespa 굿즈상품 리스트</h2>
+  <jpa-board-list2 :boards="boards"/>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import JpaBoardList2 from "@/components/boards2/JpaBoardList2";
+
+
+export default {
+  name: "JpaAespaListView.vue",
+  components: {
+    JpaBoardList2
+  },
+  computed: {
+    ...mapState([
+      'boards'
+    ]),
+  },
+  mounted() {
+    this.requestBoardListFromSpring2()
+  },
+  methods: {
+    ...mapActions([
+      'requestBoardListFromSpring2'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
