3252a7e8167e98bf00c65fd83576a9e4745c6d7b
Hanchaeseon <mdmodina34@gmail.com>
2022-09-23 20:33:15 +0900

    [한채선] 2022/09/23 f) 게시물 읽기 화면에서 관리자만 접근 가능한 수정 버튼 추가 [PRODUCT-BOARD-READ-1]

--- 한채선/vue/frontend/src/views/productBoard/ProductBoardReadView.vue
+++ 한채선/vue/frontend/src/views/productBoard/ProductBoardReadView.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="productBoardRead">
-    <h2>게시글 상세보기</h2>
+    <h2>상품 상세보기</h2>
+    <router-link :to=" { name : 'ProductBoardMainView' } "> 돌아가기 </router-link>
     <product-board-read v-if="productBoard" :productBoard="productBoard"/>
     <p v-else>Now Loading......</p>
-    <button v-if="isAdmin" @click="deleteThisProduct(productNo)">상품 삭제</button>
+    <table v-if="isAdmin">
+      <tr>
+        <td>
+          <button @click="toModifyThisProductBoard">수정</button>
+        </td>
+        <td>&nbsp;</td>
+        <td>
+          <button @click="deleteThisProduct(productNo)">삭제</button>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -13,16 +24,18 @@ import {mapActions, mapState} from "vuex";
 
 export default {
   name: "ProductBoardReadView",
-  components: {ProductBoardRead},
+  components: { ProductBoardRead },
   props: {
     productNo: {
       type: String,
       required: true,
     }
   },
+
   computed: {
     ...mapState(['productBoard', 'isAdmin'])
   },
+
   methods: {
     ...mapActions(['requestProductBoardRead', 'requestDeleteThisProductBoardToSpring']),
 
@@ -31,11 +44,20 @@ export default {
       await this.$router.push({
         name: 'ProductBoardMainView'
       })
+    },
+
+    toModifyThisProductBoard() {
+      this.$router.push( {
+        name: 'ProductBoardModifyView',
+        params: { productNo: this.productNo }
+      })
     }
   },
+
   created() {
     this.requestProductBoardRead(this.productNo)
-  }
+  },
+
 }
 </script>
 
