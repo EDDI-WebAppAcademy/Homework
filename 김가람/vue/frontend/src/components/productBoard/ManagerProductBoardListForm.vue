43401e21cc3948a415d778e6c2e6c367154dafa4
Karam Kim <kim.ram454@gmail.com>
2022-09-23 20:30:53 +0900

    [김가람] 2022/09/22 (VUE)관리자용 상품게시판 목록 구현 완료 [PRODUCT-BOARD]

---
+++ 김가람/vue/frontend/src/components/productBoard/ManagerProductBoardListForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h3 align="center">상품 목록(관리자)</h3>
+    <table border="1" align="center">
+      <tr>
+        <td align="center" width="100">상품번호</td>
+        <td align="center" width="250">상품명</td>
+      </tr>
+      <tr v-if="!products || (Array.isArray(products) && products.length === 0)">
+        <td align="center" colspan="2">
+          등록된 상품이 없습니다.
+        </td>
+      </tr>
+      <tr v-else v-for="product in products" :key="product.productNo">
+        <td align="center">
+          {{ product.productNo }}
+        </td>
+        <td align="center">
+          <!-- 상품명을 누르면 상품 상세페이지로 이동할 수 있게 router link 필요 -->
+          {{ product.name }}
+        </td>
+      </tr>
+    </table>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "ManagerProductBoardListForm",
+  props: {
+    products: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 김가람/vue/frontend/src/views/productBoard/ManagerProductBoardListView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="product" align="center">
+    <manager-product-board-list-form :products="products"/>
+    <router-link :to="{ name: 'productBoardRegisterView'}">
+      상품 등록
+    </router-link>
+  </div>
+</template>
+
+<script>
+import ManagerProductBoardListForm from "@/components/productBoard/ManagerProductBoardListForm";
+import {mapActions, mapState} from "vuex";
+export default {
+  name: "ManagerProductBoardListView",
+  components: {ManagerProductBoardListForm},
+  computed: {
+    ...mapState([
+        'products'
+    ])
+  },
+  mounted() {
+    this.requestProductListFromSpring()
+  },
+  methods: {
+    ...mapActions([
+        'requestProductListFromSpring'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
